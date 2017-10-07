f7131775c66b2f7e2b19b7219f9df8f46edfd5a5
Sandro Zbinden <sandrozbinden@gmail.com>
2017-10-07 22:33:08 +0200

    chore: rename backend to server

--- src/main/java/ch/zbinden/engineering/elasticsearch/monitoring/domain/DeleteQueryRepository.java
+++
@@ -1,8 +0,0 @@
-package ch.zbinden.engineering.elasticsearch.monitoring.domain;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface DeleteQueryRepository extends CrudRepository<DeleteQuery, Long>, PagingAndSortingRepository<DeleteQuery, Long> {
-
-}
